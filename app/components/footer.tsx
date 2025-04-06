6537a5c127af46b39c9c99e62506517721e43051
Filipe Lima <datsfilipe.foss@proton.me>
2025-04-06 19:02:31 -0300

    feat: add contact link

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -33,6 +33,15 @@ export default function Footer() {
         <li>
           <a
             className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
+            href="mailto:contact@datsfilipe.xyz"
+          >
+            <ArrowIcon />
+            <p className="ml-2 h-7">contact</p>
+          </a>
+        </li>
+        <li>
+          <a
+            className="flex items-center transition-all hover:text-neutral-800 dark:hover:text-neutral-100"
             rel="noopener noreferrer"
             target="_blank"
             href="https://github.com/datsfilipe"
