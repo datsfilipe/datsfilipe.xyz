756fb19253df5455bd16b035a731b6d4cf806427
Filipe Lima <datsfilipe.foss@proton.me>
2025-04-06 15:15:15 -0300

    fix: warnings

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -11,10 +11,12 @@ export async function generateStaticParams() {
   }))
 }
 
-export function generateMetadata({ params }) {
-  let post = getBlogPosts().find((post) => post.slug === params.slug)
+export async function generateMetadata({ params }) {
+  const { slug } = await params
+
+  let post = getBlogPosts().find((post) => post.slug === slug)
   if (!post) {
-    return
+    return null
   }
 
   let {
@@ -51,8 +53,10 @@ export function generateMetadata({ params }) {
   }
 }
 
-export default function Blog({ params }) {
-  let post = getBlogPosts().find((post) => post.slug === params.slug)
+export default async function Blog({ params }) {
+  const { slug } = await params
+
+  let post = getBlogPosts().find((post) => post.slug === slug)
 
   if (!post) {
     notFound()
