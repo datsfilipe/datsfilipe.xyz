bb6b457419e2ba9eae9f004141c54a053d20d268
Filipe Lima <datsfilipe.foss@proton.me>
2025-04-06 18:53:27 -0300

    refactor: some styling updates & add my blog posts

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -1,4 +1,7 @@
+'use client'
+
 import Link from 'next/link'
+import { usePathname } from 'next/navigation';
 
 const navItems = {
   '/': {
@@ -7,12 +10,14 @@ const navItems = {
   '/blog': {
     name: 'blog',
   },
-  'https://vercel.com/templates/next.js/portfolio-starter-kit': {
-    name: 'deploy',
-  },
+  '/projects': {
+    name: 'projects',
+  }
 }
 
 export function Navbar() {
+  const pathname = usePathname();
+
   return (
     <aside className="-ml-[8px] mb-16 tracking-tight">
       <div className="lg:sticky lg:top-20">
@@ -22,15 +27,29 @@ export function Navbar() {
         >
           <div className="flex flex-row space-x-0 pr-10">
             {Object.entries(navItems).map(([path, { name }]) => {
+              const selected = pathname === path || pathname.includes(name);
+
               return (
                 <Link
                   key={path}
                   href={path}
-                  className="transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2 m-1"
+                  className={`transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2 m-1 overflow-hidden ${selected ? "w-6" : "w-auto"
+                    }`}
                 >
-                  {name}
+                  <span
+                    className={`transform transition-all duration-300 ${selected ? "opacity-0 scale-0" : "opacity-100 scale-100"
+                      }`}
+                  >
+                    {name}
+                  </span>
+                  <span
+                    className={`absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 transform transition-all duration-300 ${selected ? "opacity-100 scale-100" : "opacity-0 scale-0"
+                      }`}
+                  >
+                    &#x2022;
+                  </span>
                 </Link>
-              )
+              );
             })}
           </div>
         </nav>
--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,21 @@ import { BlogPosts } from 'app/components/posts'
 export default function Page() {
   return (
     <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
+      <h1 className="mb-8 text-2xl font-semibold tracking-tighter relative overflow-hidden group w-fit cursor-pointer">
+        <span className="inline-block transition-transform duration-300 transform group-hover:-translate-y-full">
+          Filipe Lima
+        </span>
+        <span className="absolute left-0 top-0 transition-transform duration-300 transform translate-y-full group-hover:translate-y-0">
+          datsfilipe
+        </span>
       </h1>
-      <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
-        Vim's keystroke commands and tabs' flexibility for personal viewing
-        preferences. This extends to my support for static typing, where its
-        early error detection ensures cleaner code, and my preference for dark
-        mode, which eases long coding sessions by reducing eye strain.`}
+      <p className="mb-4 text-justify">
+        {`I'm a 23 years old Fullstack Software Engineer from Brazil who specializes in web development.
+        Currently, I work at D3, and together we are creating the next generation of Web3 domains.`}
+      </p>
+      <p className="mb-4 text-justify">
+        {`I'm proficient with the majority of the mostly used web development tools from the Javascript/Typescript ecosystem,
+        and also have experience working with integration of web3 functionality into traditional applications.`}
       </p>
       <div className="my-8">
         <BlogPosts />
