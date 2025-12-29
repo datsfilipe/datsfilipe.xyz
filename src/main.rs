82a20f66c66e6c5d75f92de4afe3123eb2c23d2f
Filipe Lima <datsfilipe.foss@proton.me>
2025-12-29 20:02:35 -0300

    feat: single thread tcp server on 6969

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+use std::{
+    fs,
+    io::{BufReader, prelude::*},
+    net::{TcpListener, TcpStream},
+};
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:6969").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        handle_conn(stream);
+    }
+}
+
+fn handle_conn(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    let request_line: Vec<_> = http_request[0].split(" ").collect();
+    if request_line[1] == "/favicon.ico" {
+        let response = "HTTP/1.1 404 NOT FOUND";
+        stream.write_all(response.as_bytes()).unwrap()
+    }
+
+    if request_line[1] == "/" && request_line[2] == "HTTP/1.1" {
+        println!("Server responding to request for: {path:?}; using {protocol:?} protocol.", path = request_line[1], protocol = request_line[2]);
+
+        let status_line = "HTTP/1.1 200 OK";
+        let contents = fs::read_to_string("pages/home.html").unwrap();
+        let length = contents.len();
+
+        let response = format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+        stream.write_all(response.as_bytes()).unwrap();
+    } else {
+        let response = "HTTP/1.1 404 NOT FOUND";
+        stream.write_all(response.as_bytes()).unwrap();
+    }
 }
